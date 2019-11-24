130f53a233d675506d5ba4faab0a7ff5aa13c95c
kvsrh <kancharlahareen@gmail.com>
2019-11-24 13:47:37 -0800

    Update ArrayOfArrayProducts.cpp

--- ArrayOfArrayProducts.cpp
+++ ArrayOfArrayProducts.cpp
@@ -1,38 +1,40 @@
-// Find the product of numbers in an array except the number at the current index. 
+// Given an array nums of n integers where n > 1,  return an array output such that output[i] is 
+// equal to the product of all the elements of nums except nums[i].
 
 class Solution {
-
-vector<long> arrayOfArrayProducts(const vector<int>& arr) 
-{
-  vector<long> leftProduct = {};
-  vector<long> rightProduct = {};
-   
+public:
+  vector<int> productExceptSelf(vector<int>& arr) {
+  
   int arraySize = arr.size();
+   
+  vector<int> leftProduct(arraySize,1);
+  vector<int> rightProduct(arraySize, 1);
+       
+      
   int left = 1;
   int right = 1;
   
-  for (int i = 0; i < arraySize; i++) {
+ for (int i = 0; i < arraySize; i++) {
       if (i == 0) {
-          leftProduct.emplace_back(left);
+          leftProduct[i] = left;
       } else {
           left *= arr[i-1];
-          leftProduct.emplace_back(left);
+          leftProduct[i] = left;
       }
-  }
-  
-  for (int i = arraySize; i < 0; i--) {
-      if (i == arraySize) {
-          rightProduct.emplace_back(right);
-      } else {
+ }
+     for (int i = arraySize-1; i >= 0; i--) {
+         if (i == arraySize-1) {
+          rightProduct[i] = right;
+        } else {
           right *= arr[i+1];
-          rightProduct.emplace_back(right);
-      }
-  }
-  
+          rightProduct[i] = right;
+        }
+    }
+      
   for (int i = 0; i < arraySize; i++) {
       leftProduct[i] = leftProduct[i]*rightProduct[i];
   }
   
   return leftProduct;
-}
+}    
 };
