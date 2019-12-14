a524345e201261eb85d700da64b0e2385297fae0
kvsrh <kancharlahareen@gmail.com>
2019-12-14 15:50:43 -0800

    Update kthLargestElement.cpp

--- kthLargestElement.cpp
+++ kthLargestElement.cpp
@@ -20,3 +20,32 @@ int findKthLargest(vector<int>& a, int k) {
         }
         return heap.top();
     }
+
+## My solution ##
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        // Using a priority queue we can keep a context of all the top k elements. 
+        
+        // Create a min-heap 
+        
+        int size = nums.size();
+        
+        priority_queue<int, vector<int>, greater<int>> pq; 
+        
+        //pq.push(nums[0]);
+        
+        for (int i = 0; i < size; i++) {
+            if (pq.size() < k) {
+                pq.push(nums[i]);
+            } else if (nums[i] > pq.top()) {
+                pq.pop();
+                pq.push(nums[i]);
+            }
+        }
+        
+        cout << "size - " << pq.size() << endl;
+        return pq.top();
+    }
+};
