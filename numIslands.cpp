47b04644984697fa9291127aa838d5995d70996f
kvsrh <kancharlahareen@gmail.com>
2020-07-05 19:56:32 -0700

    Update numIslands.cpp

--- numIslands.cpp
+++ numIslands.cpp
@@ -35,3 +35,48 @@ public:
         return num_islands;
     }
 };
+
+class Solution {
+    public: 
+    vector<int> dirs = {0, 1, 0, -1, 0};
+    
+    void dfs (vector<vector<char>> & grid, int r, int  c) {
+        grid[r][c] = '0';
+        
+        int rows = grid.size();
+        int cols = grid[0].size();
+        
+        for (int i = 0; i < dirs.size()-1; i++) {
+            int nr = r + dirs[i];
+            int nc = c + dirs[i+1];
+            if (nr >= 0 && nr < rows &&
+                nc >= 0 && nc < cols &&
+                grid[nr][nc] == '1') {
+                dfs (grid, nr, nc);
+            }
+        }
+        return;
+    }
+    
+    int numIslands (vector<vector<char>> & input) {
+        
+        int rows = input.size();
+        if (rows == 0)
+            return 0;
+        int cols = input[0].size();
+        
+        int count = 0;
+        
+        vector<vector<char>> grid = input;
+        
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == '1') {
+                    dfs (grid, r, c);
+                    count++;
+                } 
+            }
+        }
+        return count;
+    }
+};
